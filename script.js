eac08cddbb7f3cc3eb09926aa900387ca4418bf2
Penugonda <sundara.k.penugonda@accenture.com>
2023-01-20 16:20:26 +0530

    1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+let textentered=document.getElementById("evaluatedText")
+let count=document.getElementById("letterCount")
+textentered.addEventListener("keydown", calculate)
+
+let countvalue=0
+function calculate(e){
+    countvalue++
+    count.innerText=countvalue
+}
