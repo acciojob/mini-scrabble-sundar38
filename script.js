7099978a1c0e390035d575d124c48c59f786adc2
Penugonda <sundara.k.penugonda@accenture.com>
2023-01-20 17:30:25 +0530

    3

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-let textentered=document.getElementById("evaluatedText")
-let count=document.getElementById("letterCount")
-textentered.addEventListener("keydown", calculate)
+let count;
+document.getElementById("evaluatedText").value = ""; // initially string is empty so count is also empty
 
-let countvalue=0
-function calculate(e){
-    countvalue++
-    count.innerText=countvalue
-}
+document.addEventListener("keyup", function () {
+  count = document.getElementById("evaluatedText").value; //here this value will return string
+  document.querySelector("#letterCount").textContent = count.length;
+  //   console.log(count);
+});
